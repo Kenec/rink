0c1a113fe2edc38123b3770d652651d2708a476f
Kenec <kenechukwunnamani@Kenechukwus-MacBook-Pro.local>
2020-12-12 20:13:41 +0100

    ready command line argument

--- main.go
+++ main.go
@@ -7,23 +7,37 @@ import (
 	"net/url"
 	"net/http"
 	"net/http/httputil"
+	"flag"
+	"os"
 )
 func main(){
 	// read the config file
 	fmt.Println("Reading Rink Conf File Done!")
+
+	domain := flag.String("domain", "", "domain name to add to /etc/hosts")
+	port := flag.String("port", "", "PORT on which the application is running on!")
+	flag.Parse()
+
+	if *domain == "" || *port == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	// edit the /etc/host file
 	hosts, err := txeh.NewHostsDefault()
 	if err != nil {
 		panic(err)
 	}
-	hosts.AddHost("127.0.0.1", "dev.devito.com")
+	hosts.AddHost("127.0.0.1", *domain)
 	hosts.Save()
+
+	//TODO: check if the user set the SSL to TRUE. In which case, form the URL with https.
+	fullDomain := "http://" + *domain + ":" + *port
 
 
 	// proxy traffic to the local web app
 	fmt.Println("Proxing to the localhost running on PORT!")
-	local, err := url.Parse("http://dev.devito.com:3000")
+	local, err := url.Parse(fullDomain)
 	if err != nil {
 		panic(err)
 	}
